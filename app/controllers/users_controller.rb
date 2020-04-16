c2792f4f3e9f9dda75a8f89609e46cb583c8e8b6
Yni-05 <yhforwork1@gmail.com>
2020-04-16 10:07:30 +0900

    ログアウト機能の実装、users_controllerの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    if current_user.update(user_parms)
+    if current_user.update(user_params)
       redirect_to root_path
     else
       render :edit
