8a15b6655f926068d2474c718e5abe1b6e7d1c93
Yni-05 <yhforwork1@gmail.com>
2020-04-23 16:22:14 +0900

    投稿されたメッセージ、画像の表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,7 +9,7 @@ class MessagesController < ApplicationController
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-      redirect_to group_messages_path(@gorup), notice: 'メッセージが送信されました'
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
     else
       @messages = @group.mssages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
@@ -19,7 +19,7 @@ class MessagesController < ApplicationController
 
   private
 
-  def messege_params
+  def message_params
     params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
 
